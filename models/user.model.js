eaf25af4fcd47e034127ab3022f2be7c73e61ac0
Saidamir <rsr79970@gmail.com>
2026-04-22 18:52:38 +0500

    server

---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
+});
+
+export const User = model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,28 @@
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+const app = express();
+const port = 3000;
+
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => {
+  res.send("Server is LIVE!");
+});
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
