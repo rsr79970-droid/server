a9126199c0730bbd7a492e1da36231b69f68ddc5
Saidamir <rsr79970@gmail.com>
2026-05-06 22:43:01 +0500

    fix clerk client and server

--- server.js
+++ server.js
@@ -1,39 +1,59 @@
 import dns from "dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+
 import { connectDB } from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
+
 import { inngest, functions } from "./inngest/index.js";
+
 import showRouter from "./routes/showRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+
 import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 const app = express();
-const port = 3000;
 
 await connectDB();
 
-app.use('/api/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(
+  "/api/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhooks,
+);
 
 app.use(express.json());
-app.use(cors());
+
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
   res.send("Server is LIVE!");
 });
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  }),
+);
+
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/user", userRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
-
+export default app;
