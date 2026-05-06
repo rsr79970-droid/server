3caa12d4501cfabfa1ae4eb598454a8fec762019
Saidamir <rsr79970@gmail.com>
2026-05-06 21:13:02 +0500

    fix vercel config

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -1,7 +1,5 @@
 import nodemailer from "nodemailer";
 
-const nodemailer = require("nodemailer");
-
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
